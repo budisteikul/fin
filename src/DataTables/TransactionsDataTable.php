2bfbb2111a6cbf5fb45107f5ae1ab45f95a44199
Budi <aku@budi.my.id>
2022-11-30 00:51:24 +0700

    1

--- src/DataTables/TransactionsDataTable.php
+++ src/DataTables/TransactionsDataTable.php
@@ -34,7 +34,7 @@ class TransactionsDataTable extends DataTable
                 return number_format($id->amount, 0, ',', '.');
             })
 			->addColumn('action', function ($id) {
-				return '<div class="btn-toolbar justify-content-end"><div class="btn-group mr-2 mb-2" role="group"><button id="btn-edit" type="button" onClick="EDIT(\''.$id->id.'\'); return false;" class="btn btn-sm btn-success"><i class="fa fa-edit"></i> Edit</button><button id="btn-del" type="button" onClick="DELETE(\''. $id->id .'\')" class="btn btn-sm btn-danger"><i class="fa fa-trash-alt"></i> Delete</button></div><div class="btn-group mb-2" role="group"></div></div>';
+				return '<div class="btn-toolbar justify-content-end"><div class="btn-group mr-2 mb-0" role="group"><button id="btn-edit" type="button" onClick="EDIT(\''.$id->id.'\'); return false;" class="btn btn-sm btn-success"><i class="fa fa-edit"></i> Edit</button><button id="btn-del" type="button" onClick="DELETE(\''. $id->id .'\')" class="btn btn-sm btn-danger"><i class="fa fa-trash-alt"></i> Delete</button></div><div class="btn-group mb-2" role="group"></div></div>';
             })
 			->rawColumns(['action']);
     }
