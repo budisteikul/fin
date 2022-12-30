d01245135d27e0172c852c8c93e5545d1ff70c66
Budi <aku@budi.my.id>
2022-12-30 22:08:00 +0700

    Update TestController.php

--- src/Controllers/TestController.php
+++ src/Controllers/TestController.php
@@ -7,11 +7,23 @@ use Illuminate\Http\Request;
 use budisteikul\toursdk\Helpers\WiseHelper;
 use budisteikul\fin\Models\fin_transactions;
 
+use budisteikul\toursdk\Models\ShoppingcartProduct;
+use Ramsey\Uuid\Uuid;
+
 class TestController extends Controller
 {
     public function test()
     {
-        print_r(config('filesystems.disks.local.root'));
+        $shoppingcart_products = ShoppingcartProduct::whereHas('shoppingcart', function ($query) {
+                return $query->where('booking_status','CONFIRMED');
+        })->whereNotNull('date')->where('date', '<', date('Y-m-d H:i:s') )->orderBy('date', 'DESC')->get();
+        foreach($shoppingcart_products as $shoppingcart_product)
+        {
+            print_r($shoppingcart_product->shoppingcart->confirmation_code .'<br />');
+            print_r($shoppingcart_product->shoppingcart->session_id .'<br />');
+            $shoppingcart_product->shoppingcart->session_id = Uuid::uuid4()->toString();
+            $shoppingcart_product->shoppingcart->save();
+        }
     }
 
     public function test___()
