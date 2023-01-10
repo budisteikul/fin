ab4c8c8bc1003a1ecf9f2f330099db4623abeea1
Budi <aku@budi.my.id>
2023-01-10 10:14:31 +0700

    1

--- src/DataTables/TransferDataTable.php
+++ src/DataTables/TransferDataTable.php
@@ -57,7 +57,7 @@ class TransferDataTable extends DataTable
 
                 return '<div class="btn-toolbar justify-content-end"><div class="btn-group mr-2 mb-0" role="group">
                 
-                <button id="btn-del" type="button" onClick="DELETE(\''. $id->id .'\')" class="btn btn-sm btn-danger"><i class="fa fa-trash-alt"></i> Delete</button> 
+                <button id="btn-del" type="button" onClick="DELETE(\''. $id->id .'\')" class="btn btn-sm btn-danger pt-0 pb-0 pl-1 pr-1"><i class="fa fa-trash-alt"></i> Delete</button> 
                 </div><div class="btn-group mb-2" role="group"></div></div>';
             })
             ->rawColumns(['action','status'])
