88bd15436a0a6e3b531020d82f03fbe008b28360
Budi <aku@budi.my.id>
2023-01-19 16:13:00 +0700

    Update TestController.php

--- src/Controllers/TestController.php
+++ src/Controllers/TestController.php
@@ -10,12 +10,15 @@ use budisteikul\fin\Models\fin_transactions;
 use budisteikul\toursdk\Models\ShoppingcartProduct;
 use Ramsey\Uuid\Uuid;
 use budisteikul\toursdk\Helpers\FirebaseHelper;
+use budisteikul\toursdk\Helpers\XenditHelper;
 
 class TestController extends Controller
 {
     public function test(Request $request)
     {
-        
+        $xendit = new XenditHelper();
+        $aaa = $xendit->createEWalletDanaCharge(10000,'https://webhook.site/f6a7dccc-8e3b-4ab1-a64c-556d3cc91ec8');
+        print_r($aaa);
         
     }
 
