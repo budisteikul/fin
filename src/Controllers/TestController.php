c968c86029e4dc802fdabe4af5f1c5c3bc0757e4
Budi <aku@budi.my.id>
2023-02-25 01:41:50 +0700

    Update TestController.php

--- src/Controllers/TestController.php
+++ src/Controllers/TestController.php
@@ -20,7 +20,7 @@ class TestController extends Controller
     public function test(Request $request)
     {
         $tw = New WiseHelper();
-        $aaa = $tw->getBalanceAccounts("USD");
+        $aaa = $tw->getRecipientAccounts();
         print_r($aaa);
     }
     
