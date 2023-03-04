d2eddba1c43f17ca3329a68144ecc1412b125926
Budi <aku@budi.my.id>
2023-03-04 16:49:47 +0700

    Update menu.blade.php

--- src/views/layouts/menu.blade.php
+++ src/views/layouts/menu.blade.php
@@ -3,25 +3,25 @@
       <hr class="sidebar-divider my-0">
       <li class="nav-item 
       
-        {{ (request()->is('cms/fin/transactions*')) ? 'active' : '' }}
+        
         {{ (request()->is('cms/fin/profitloss*')) ? 'active' : '' }}
         {{ (request()->is('cms/fin/banking*')) ? 'active' : '' }}
         {{ (request()->is('cms/fin/payment*')) ? 'active' : '' }}
-        {{ (request()->is('cms/fin/transfer*')) ? 'active' : '' }}
+        
       
       ">
       @php
         $collapsed = 'collapsed';
         $show = '';        
-        if(request()->is('cms/fin/transactions*') || request()->is('cms/fin/profitloss*') || request()->is('cms/fin/banking*') || request()->is('cms/fin/payment*') || request()->is('cms/fin/transfer*'))
+        if(request()->is('cms/fin/profitloss*') || request()->is('cms/fin/banking*') || request()->is('cms/fin/payment*'))
         {
           $collapsed = '';
           $show = 'show';
         }
       @endphp
         <a class="nav-link {{$collapsed}}" href="#" data-toggle="collapse" data-target="#menu-fin" aria-expanded="false" aria-controls="menu-fin">
-          <i class="fas fa-balance-scale"></i>
-          <span>FINANCIAL</span>
+          <i class="fas fa-signal"></i>
+          <span>REPORT</span>
         </a>
         <div id="menu-fin" class="collapse {{$show}}" aria-labelledby="heading1" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
@@ -32,6 +32,35 @@
 
             <a class="collapse-item {{ (request()->is('cms/fin/banking*')) ? 'active' : '' }}" href="{{ route('route_fin_banking.index') }}"><i class="far fa-circle"></i> {{ __('Banking') }}</a>
 
+          </div>
+        </div>
+      </li>
+      
+ <!-- ##################################################################### -->
+ <!-- ##################################################################### -->
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item 
+      
+        {{ (request()->is('cms/fin/transactions*')) ? 'active' : '' }}
+        {{ (request()->is('cms/fin/transfer*')) ? 'active' : '' }}
+      
+      ">
+      @php
+        $collapsed = 'collapsed';
+        $show = '';        
+        if(request()->is('cms/fin/transactions*') || request()->is('cms/fin/transfer*'))
+        {
+          $collapsed = '';
+          $show = 'show';
+        }
+      @endphp
+        <a class="nav-link {{$collapsed}}" href="#" data-toggle="collapse" data-target="#menu-tr" aria-expanded="false" aria-controls="menu-tr">
+          <i class="fas fa-balance-scale"></i>
+          <span>FINANCIAL</span>
+        </a>
+        <div id="menu-tr" class="collapse {{$show}}" aria-labelledby="heading1" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            
             <a class="collapse-item {{ (request()->is('cms/fin/transactions*')) ? 'active' : '' }}" href="{{ route('route_fin_transactions.index') }}"><i class="far fa-circle"></i> {{ __('Transaction') }}</a>
             
             <a class="collapse-item {{ (request()->is('cms/fin/transfer*')) ? 'active' : '' }}" href="{{ route('route_fin_transfer.index') }}"><i class="far fa-circle"></i> {{ __('Transfer') }}</a>
