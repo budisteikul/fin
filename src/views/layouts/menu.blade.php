c5c23a0f7d8e6364f0726d984d6f6085dd6ddc13
Budi <aku@budi.my.id>
2024-03-10 15:02:30 +0700

    Update menu.blade.php

--- src/views/layouts/menu.blade.php
+++ src/views/layouts/menu.blade.php
@@ -4,10 +4,40 @@
       <li class="nav-item 
       
         {{ (request()->is('cms/fin/report/monthly*')) ? 'active' : '' }}
-        {{ (request()->is('cms/fin/report/asset*')) ? 'active' : '' }}
+      
+      ">
+      @php
+        $collapsed = 'collapsed';
+        $show = '';        
+        if(request()->is('cms/fin/report/monthly*'))
+        {
+          $collapsed = '';
+          $show = 'show';
+        }
+      @endphp
+        <a class="nav-link {{$collapsed}}" href="#" data-toggle="collapse" data-target="#menu-fin" aria-expanded="false" aria-controls="menu-fin">
+          <i class="fas fa-chart-line"></i>
+          <span>REPORT</span>
+        </a>
+        <div id="menu-fin" class="collapse {{$show}}" aria-labelledby="heading1" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            
+            <a class="collapse-item {{ (request()->is('cms/fin/report/monthly*')) ? 'active' : '' }}" href="{{ route('route_fin_report_monthly.index') }}"><i class="far fa-circle"></i> {{ __('Monthly Report') }}</a>
+
+             
+          </div>
+        </div>
+      </li>
+      
+ <!-- ##################################################################### -->
+ <!-- ##################################################################### -->
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item 
+      
+        
+        
         {{ (request()->is('cms/fin/profitloss*')) ? 'active' : '' }}
         {{ (request()->is('cms/fin/tax*')) ? 'active' : '' }}
-        {{ (request()->is('cms/fin/banking*')) ? 'active' : '' }}
         {{ (request()->is('cms/fin/transactions*')) ? 'active' : '' }}
         {{ (request()->is('cms/fin/neraca*')) ? 'active' : '' }}
       
@@ -15,24 +45,24 @@
       @php
         $collapsed = 'collapsed';
         $show = '';        
-        if(request()->is('cms/fin/report/asset*') || request()->is('cms/fin/report/monthly*') || request()->is('cms/fin/profitloss*') || request()->is('cms/fin/tax*') || request()->is('cms/fin/banking*') || request()->is('cms/fin/transactions*') || request()->is('cms/fin/neraca*'))
+        if(request()->is('cms/fin/profitloss*') || request()->is('cms/fin/tax*') || request()->is('cms/fin/transactions*') || request()->is('cms/fin/neraca*'))
         {
           $collapsed = '';
           $show = 'show';
         }
       @endphp
-        <a class="nav-link {{$collapsed}}" href="#" data-toggle="collapse" data-target="#menu-fin" aria-expanded="false" aria-controls="menu-fin">
+        <a class="nav-link {{$collapsed}}" href="#" data-toggle="collapse" data-target="#menu-fin1" aria-expanded="false" aria-controls="menu-fin1">
           <i class="fas fa-balance-scale"></i>
-          <span>FINANCIAL</span>
+          <span>ACCOUNTING</span>
         </a>
-        <div id="menu-fin" class="collapse {{$show}}" aria-labelledby="heading1" data-parent="#accordionSidebar">
+        <div id="menu-fin1" class="collapse {{$show}}" aria-labelledby="heading1" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             
-            <a class="collapse-item {{ (request()->is('cms/fin/report/monthly*')) ? 'active' : '' }}" href="{{ route('route_fin_report_monthly.index') }}"><i class="far fa-circle"></i> {{ __('Monthly Report') }}</a>
+            
 
              <a class="collapse-item {{ (request()->is('cms/fin/transactions*')) ? 'active' : '' }}" href="{{ route('route_fin_transactions.index') }}"><i class="far fa-circle"></i> {{ __('Transaction') }}</a>
             
-             @if(env('APP_ENV')=="local")
+             
              
              
              <a class="collapse-item {{ (request()->is('cms/fin/tax*')) ? 'active' : '' }}" href="{{ route('route_fin_tax.index') }}"><i class="far fa-circle"></i> {{ __('Tax PP23') }}</a>
@@ -43,7 +73,7 @@
 
              <a class="collapse-item {{ (request()->is('cms/fin/neraca*')) ? 'active' : '' }}" href="{{ route('route_fin_neraca.index') }}"><i class="far fa-circle"></i> {{ __('Balance Sheet') }}</a>
 
-             @endif
+            
           </div>
         </div>
       </li>
